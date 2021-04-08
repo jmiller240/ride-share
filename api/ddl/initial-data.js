e3806715fad66109eaf9198100fe44657affa1a0
Noah Gegner <noahgegner@macbook-pro.campus.tayloru.edu>
2021-04-08 15:41:56 -0400

    added config file

--- api/ddl/initial-data.js
+++ api/ddl/initial-data.js
@@ -62,57 +62,57 @@ exports.seed = function (knex, Promise) {
                             state: [{ '#ref': 'IN' }],
                             zipCode: '46077'
                         }]
-                        
+
                     }]
                 }]
             },
-            { '#id': 'Tim', firsName: 'Tim', lastName: 'Bucktwo' , email: 'tim@gmail.com', password: '1234567', phone: '555-555-5555' , isAdmin: true,
-                passenger: [{ 
+            { '#id': 'Tim', firsName: 'Tim', lastName: 'Bucktwo', email: 'tim@gmail.com', password: '1234567', phone: '555-555-5555', isAdmin: true,
+                passenger: [{
                     ride: [{
                         '#id': "Bob's Ride",
-                         date: '2021-04-10', time: '15:30:00', distance: 70.3, fuelPrice: 2.87, fee: 20,
-                         Vehicle: [{
-                             '#id': 'Ford' ,make: 'Ford', model: 'Explorer', color: 'maroon', capacity: 6, mpg: 26.5, licensePlate: '4W43IO',
-                             vehicleType: [{
-                                 type: 'SUV'
-                             }],
-                             licenseState: [{
-                                 '#ref': 'IN'
-                             }],
-                         }],
-                         fromLocationID: [{
-                             name: 'Wengatz Hall',
-                             address: '45 Reade ave',
-                             city: 'Upland',
-                             state: [{ '#ref': 'IN' }],
-                             zipcode: '46989'
-                         }],
-                         toLocationID: [{
-                             name: 'Wheaton University',
-                             address: '284 Wheaton ave',
-                             city: 'Wheaton',
-                             state: [{
-                                 '#id': 'IL',
-                                 abbreviation: 'IL',
-                                 name: 'Illinois',
-                             }]
-                         }]
+                        date: '2021-04-10', time: '15:30:00', distance: 70.3, fuelPrice: 2.87, fee: 20,
+                        Vehicle: [{
+                            '#id': 'Ford', make: 'Ford', model: 'Explorer', color: 'maroon', capacity: 6, mpg: 26.5, licensePlate: '4W43IO',
+                            vehicleType: [{
+                                type: 'SUV'
+                            }],
+                            licenseState: [{
+                                '#ref': 'IN'
+                            }],
+                        }],
+                        fromLocationID: [{
+                            name: 'Wengatz Hall',
+                            address: '45 Reade ave',
+                            city: 'Upland',
+                            state: [{ '#ref': 'IN' }],
+                            zipcode: '46989'
+                        }],
+                        toLocationID: [{
+                            name: 'Wheaton University',
+                            address: '284 Wheaton ave',
+                            city: 'Wheaton',
+                            state: [{
+                                '#id': 'IL',
+                                abbreviation: 'IL',
+                                name: 'Illinois',
+                            }]
+                        }]
                     }]
                 }]
             },
             { '#id': 'Susie', firstName: 'Susie', lastName: 'Jacobs', email: 'sue@yahoo.com', password: '343463134', phone: '555-234-1264', isAdmin: false,
-                    driver: [{
-                        licenseNumber: '123456',
-                        licenseState: [{ '#ref': 'OH' }],
-                        authorization: [{
-                            vehicleID: [{'#ref': 'Buick'}]
-                        }],
-                        drivers: [{
-                            rideID: [{'#ref': '10'}]
-                        }]
+                driver: [{
+                    licenseNumber: '123456',
+                    licenseState: [{ '#ref': 'OH' }],
+                    authorization: [{
+                        vehicleID: [{ '#ref': 'Buick' }]
+                    }],
+                    drivers: [{
+                        rideID: [{ '#ref': '10' }]
                     }]
-                },
-            { '#id': 'Bob', firstName: 'Bob', lastName: 'Allen', email: 'bob@bob.edu', password: '4928598209', phone: '555-589-0934', isAdmin: true, 
+                }]
+            },
+            { '#id': 'Bob', firstName: 'Bob', lastName: 'Allen', email: 'bob@bob.edu', password: '4928598209', phone: '555-589-0934', isAdmin: true,
                 Driver: [{
                     licenseNumber: '5496-483-4033',
                     licenseState: [{
@@ -126,5 +126,6 @@ exports.seed = function (knex, Promise) {
                     }]
                 }]
             }
-        ])       
-    )};
+        ])
+    )
+};
