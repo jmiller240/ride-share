df6ce0b51e4e0d3a23f495eb58b580284f2a9af6
jmiller240 <millerjd125@gmail.com>
2021-04-14 09:50:54 -0400

    knex working

--- api/test/test-user.js
+++ api/test/test-user.js
@@ -1,11 +1,10 @@
-require("./init");
+const { knex } = require("./init");
 
+//const { default: knex } = require("knex");
 const User = require("../models/User.js");
 
 async function create() {
-  const newUser = await User.query().select("id", "firstName");
-
-  /*.insert(
+  const newUser = await User.query().insert(
         {
             firstName: 'Pat',
             lastName: 'Rick',
@@ -14,22 +13,32 @@ async function create() {
             phone: '209-456-3201',
             isAdmin: false,
         }
-    )*/
+    )
   console.log("CREATE\n", newUser);
 }
 
-async function read() {}
+async function read(id) {
+  const user = await User.query().select().where('id', id);
+
+  console.log('READ\n', user);
+
+}
 
 async function update() {}
 
-async function deleteTest() {}
+async function deleteTest(id) {
+  const user = await User.query().del().where('id', 10);
+
+  console.log('DELETE\n', user);
+}
 
 async function main() {
-  create();
-  /*read();
-    update();
-    deleteTest();
-    */
+  //await create();
+  //await read(8);
+  await update();
+  //await deleteTest();
+  
+   knex.destroy();
 }
 
 process.on("unhandledRejection", (err) => {
